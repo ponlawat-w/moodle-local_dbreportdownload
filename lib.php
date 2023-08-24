958cfaa41ebfae403f9a8ac94cb01d1ec64b6082
Ponlawat W. <ponlawat_w@cmu.ac.th>
2023-08-24 14:10:54 +0700

    Added download as selected fields

--- lib.php
+++ lib.php
@@ -1,6 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') or die();
 
+require_once(__DIR__ . '/../../mod/data/classes/manager.php');
+require_once(__DIR__ . '/../../mod/data/classes/template.php');
+
 function local_dbreportdownload_extend_settings_navigation(settings_navigation $settings, context $context) {
     if (!($context instanceof context_module)) {
         return;
@@ -17,4 +20,26 @@ function local_dbreportdownload_extend_settings_navigation(settings_navigation $
         new moodle_url('/local/dbreportdownload/options.php', ['d' => $cm->instance]), null, null, 'modulesettings'
     );
     $node->set_show_in_secondary_navigation(false);
+}
+
+/**
+ * @param stdClass $dataid
+ * @param string[] $fieldids
+ * @return \mod_data\template
+ */
+function local_dbreportdownload_gettemplate($data, $fieldids) {
+    /**
+     * @var \moodle_database $DB
+     */
+    global $DB;
+
+    $contents = [];
+    foreach ($fieldids as $fieldid) {
+        $field = $DB->get_record('data_fields', ['id' => $fieldid], 'id,name', MUST_EXIST);
+        $contents[] = "<strong>{$field->name}</strong>: [[{$field->name}]]";
+    }
+
+    $content = implode('<br>', $contents);
+
+    return new \mod_data\template(\mod_data\manager::create_from_instance($data), $content);
 }
