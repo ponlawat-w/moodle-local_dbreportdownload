7249a99aaea3860cfa1d1f65850c90f63e232705
Ponlawat W. <ponlawat_w@cmu.ac.th>
2023-10-20 12:33:31 +0700

    Added word count

--- lib.php
+++ lib.php
@@ -65,7 +65,7 @@ function local_dbreportdownload_gettemplate($data, $fieldids) {
 
     foreach ($fieldids as $fieldid) {
         $field = $DB->get_record('data_fields', ['id' => $fieldid], 'id,type,name', MUST_EXIST);
-        $column = $field->type === 'textarea' ? "{$field->name} ([[[WORD_COUNT({$field->name})]]])" : $field->name;
+        $column = $field->type === 'textarea' ? "{$field->name}<br>([[{$field->name}#wordCount]])" : $field->name;
         $contents[] = "<tr><th>{$column}</th><td>[[{$field->name}]]</td></tr>";
     }
 
@@ -74,4 +74,51 @@ function local_dbreportdownload_gettemplate($data, $fieldids) {
     $html .= '</tbody></table>';
 
     return new \mod_data\template(\mod_data\manager::create_from_instance($data), $html);
+}
+
+/**
+ * @param string $content
+ * @return int
+ */
+function local_dbreportdownload_countwords($content) {
+    $content = preg_replace("(<.*?>|&nbsp;|\\\\n)", ' ', $content);
+    $words = explode(' ', $content);
+    $count = 0;
+    foreach ($words as $word) {
+        if (trim($word)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+/**
+ * @param \mod_data\template $parser
+ * @param stdClass $record
+ * @return string
+ */
+function local_dbreportdownload_parseentry($parser, $record) {
+    /**
+     * @var \moodle_database $DB
+     */
+    global $DB;
+    $parsed = $parser->parse_entries([$record]);
+
+    $finds = [];
+    $replaces = [];
+    $textareas = $DB->get_records('data_fields', ['dataid' => $record->dataid, 'type' => 'textarea']);
+    foreach ($textareas as $textarea) {
+        $find = "[[{$textarea->name}#wordCount]]";
+        if (!str_contains($parsed, $find)) {
+            continue;
+        }
+        $content = $DB->get_record('data_content', ['fieldid' => $textarea->id, 'recordid' => $record->id]);
+        $wordcount = $content ? local_dbreportdownload_countwords($content->content) : 0;
+        $finds[] = $find;
+        $replaces[] = get_string($wordcount === 1 ? 'wordcount_singular' : 'wordcount_plural', 'local_dbreportdownload', $wordcount);
+    }
+
+    $parsed = str_ireplace($finds, $replaces, $parsed);
+
+    return $parsed;
 }
